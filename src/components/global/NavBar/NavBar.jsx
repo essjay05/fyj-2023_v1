948d07205ff37b840258c4b213b4209b3bf79168
Joy Serquiña <stepjoyous@gmail.com>
2023-01-10 23:01:56 -0800

    Updated Navbar styles.

--- src/components/global/NavBar/NavBar.jsx
+++ src/components/global/NavBar/NavBar.jsx
@@ -4,28 +4,28 @@ import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <nav className='d-flex'>
+    <nav className='d-flex justify-content-around align-items-center'>
       <NavLink 
         to='/'
-        className='handwritten-style title-style-6 mx-3 nav-link'
+        className='handwritten-style mx-3 nav-link'
         aria-label='Home'>
           Home
       </NavLink>
       <NavLink 
         to='/portfolio'
-        className='handwritten-style title-style-6 mx-3 nav-link'
+        className='handwritten-style mx-3 nav-link'
         aria-label='Portfolio'>
           Portfolio
       </NavLink>
       <NavLink 
         to='/about'
-        className='handwritten-style title-style-6 mx-3 nav-link'
+        className='handwritten-style mx-3 nav-link'
         aria-label='About'>
           About
       </NavLink>
       <NavLink 
         to='/style-guide'
-        className='handwritten-style title-style-6 mx-3 nav-link'
+        className='handwritten-style mx-3 nav-link'
         aria-label='Style Guide'>
           Style Guide
       </NavLink>
