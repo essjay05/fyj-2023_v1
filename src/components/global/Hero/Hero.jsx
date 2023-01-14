d1baca6b2ec1bb980b38ac01fb7d8fbe1ab331fe
Joy Serquiña <stepjoyous@gmail.com>
2023-01-14 15:52:51 -0800

    Updated Hero.jsx and Hero.css so that the width would remain within the bounds of the screen and not extend over.

--- src/components/global/Hero/Hero.jsx
+++ src/components/global/Hero/Hero.jsx
@@ -2,16 +2,16 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero-img-container w-100'>
+    <div className='hero-img-container'>
       <div className='hero-img-text'>
         <h1 className='handwritten-style'>Joy</h1>
         <p>\'joi\ noun</p>
         <h2 className='handwritten-style'>Definition of 'joy':</h2>
         <ul >
           <li>1: the emotion evoked by well-being,
-            <br/><span className='ml-5'>success, or good fortune or by</span>
-            <br/><span className='ml-5'>the prospect of possessing what one</span>
-            <br/><span className='ml-5'>desires.</span>
+            <br/><span>success, or good fortune or by</span>
+            <br/><span>the prospect of possessing what one</span>
+            <br/><span>desires.</span>
           </li>
           <li>
             2: a source or cause of delight.
