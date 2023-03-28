0f6518c74148d18bbe61effacd968a9289c18177
Joy Serquiña <stepjoyous@gmail.com>
2023-03-28 15:43:17 -0700

    Installed axios npm package for fetching data. Updating Portfolio page to try and retrieve projects info from Joy's Strapi Database.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,12 +1,35 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import Header from '../components/global/Header/Header'
 import Footer from '../components/global/Footer/Footer'
 
 export function Portfolio({ pageName }) {
 
+  const [ projects, setProjects ] = useState(null)
+  const [ loading, setLoading ] = useState(true)
+
+  const projectsUrl = `http://localhost:1337/api/projects`
+
+  const getProjects = () => {
+    return new Promise(resolve => {
+      axios(projectsUrl)
+        .then(response => {
+          const projectsResults = response.data.results
+          console.log(projectsResults)
+          setProjects(projectsResults)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+        .finally(() => setLoading(false))
+    })
+  }
+
+
   useEffect(() => {
     document.title = pageName
+    getProjects()
   }, [])
 
   return (
